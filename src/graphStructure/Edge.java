d31bd5d374cf9129eccdcdba940df8b2017ae790
José Antonio Céspedes Downing <tonio782009@hotmail.com>
2018-11-13 20:08:20 -0600

    Cambios múltiples al grafo, implementado Dijkstra

--- src/graphStructure/Edge.java
+++ src/graphStructure/Edge.java
@@ -9,57 +9,37 @@ package graphStructure;
  * 
  * Clase Edge (Arista)
  */
-public class Edge {
-	/**
-	 * Atributos de la clase Edge
-	 */
-	private Vertex src;
-	private Vertex dest;
-	private int weight;
-	
-	
-	/**
-	 * Constructor de la clase Edge que recibe el vertice de procedencia y el vertice al que va, a su vez de su peso.
-	 * @param src
-	 * @param dest
-	 * @param weight
-	 */
-	public Edge(Vertex src, Vertex dest, int weight) {
-		this.src = src;
-		this.dest = dest;
-		this.setWeight(weight);
-	}
-	
-	/**
-	 * Devuelve el peso de la arista
-	 * @return weight
-	 */
-	public int getWeight() {
-		return weight;
-	}
-	
-	/**
-	 * Asigna el peso de la arista.
-	 * @param weight
-	 */
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
+public class Edge  {
+    private final String id;
+    private final Vertex source;
+    private final Vertex destination;
+    private final int weight;
 
-	/**
-	 * Obtiene el vertice de origen.
-	 * @return src
-	 */
-	public Vertex getSource() {
-		return src;
-	}
-	
-	/**
-	 * Obtiene el vertice de destino.
-	 * @return dest
-	 */
-	public Vertex getDest() {
-		return dest;
-	}
-	
+    public Edge(String id, Vertex source, Vertex destination, int weight) {
+        this.id = id;
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public String getId() {
+        return id;
+    }
+    public Vertex getDestination() {
+        return destination;
+    }
+
+    public Vertex getSource() {
+        return source;
+    }
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return source + " " + destination;
+    }
+
+
 }
